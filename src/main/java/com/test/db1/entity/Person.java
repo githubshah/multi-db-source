cbbaabd1ba595449aeba69aee8e81d48a9c0bc0c
Shaid Hussain <shaid.hussain@nagarro.com>
2024-02-14 18:59:53 +0530

    configure two db source

--- src/main/java/com/test/db1/entity/Person.java
+++ src/main/java/com/test/db1/entity/Person.java
@@ -16,6 +16,7 @@ public class Person {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
--- src/main/java/com/test/db2/entity/Animal.java
+++ src/main/java/com/test/db2/entity/Animal.java
@@ -16,6 +16,7 @@ public class Animal {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
